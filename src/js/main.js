0b6c3f9e80675dda668fcb68e0c83ba9df81270f
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-16 20:26:14 +0200

    Chore: Second version

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,6 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
+import {
+  stringify
+} from "querystring";
 
 const message = document.querySelector(".form__message--js");
 const load = document.querySelector(".form__button--load-js");
@@ -24,9 +10,24 @@ save.addEventListener("click", (e) => {
   e.preventDefault();
   console.log(message.value);
   localStorage.setItem('message', message.value);
+  if (message.value) {
+    message.textContent = "";
+  }
 })
 
 load.addEventListener("click", (e) => {
   e.preventDefault();
   message.value = localStorage.getItem('message');
 })
+
+const person = {
+  name: "Piotr",
+  age: 32
+}
+const personStringify = JSON.stringify(person);
+console.log(personStringify);
+localStorage.setItem("person", personStringify);
+const newPerson = localStorage.getItem("person");
+console.log(newPerson);
+const xxx = JSON.parse(newPerson);
+console.log(xxx);
