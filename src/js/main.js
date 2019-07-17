da38a25ce328f0c538a92cb267dbbc7865ffb275
Piotr Napieralski <piotrn-87@o2.pl>
2019-07-17 20:43:30 +0200

    Fix: modified SCSS file

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-import {
-  stringify
-} from "querystring";
-
 const message = document.querySelector(".form__message--js");
 const load = document.querySelector(".form__button--load-js");
 const save = document.querySelector(".form__button--save-js");
@@ -19,15 +15,3 @@ load.addEventListener("click", (e) => {
   e.preventDefault();
   message.value = localStorage.getItem('message');
 })
-
-const person = {
-  name: "Piotr",
-  age: 32
-}
-const personStringify = JSON.stringify(person);
-console.log(personStringify);
-localStorage.setItem("person", personStringify);
-const newPerson = localStorage.getItem("person");
-console.log(newPerson);
-const xxx = JSON.parse(newPerson);
-console.log(xxx);
